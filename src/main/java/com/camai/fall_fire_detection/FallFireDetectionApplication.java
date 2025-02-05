36b13499e71fa9624dbf28d118957d39f7f5647a
Hai Luong Quoc <luongquochai0707@gmail.com>
2025-02-06 04:30:48 +0700

    Implement create

---
+++ src/main/java/com/camai/fall_fire_detection/FallFireDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.camai.fall_fire_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallFireDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallFireDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camai/fall_fire_detection/FallFireDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camai.fall_fire_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallFireDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
